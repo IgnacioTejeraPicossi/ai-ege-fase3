83cb11a662bc302df1afb08b65e1be51a4b0e265
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2025-05-16 13:12:06 +0200

    Add visores test

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   e2e: {
+    //baseUrl: 'https://www.ege.no',  // Angi URL for lokal installasjon
+    //baseUrl: 'http://localhost:8080/site/default/master/no',  // Angi URL for lokal installasjon
+    baseUrl: 'http://localhost:8080',  // Angi URL for lokal installasjon
+    viewportWidth: 1280,                  // Sett skjermstørrelse (bredde)
+    viewportHeight: 720,                  // Sett skjermstørrelse (høyde)
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/ege.cy.js
+++ cypress/e2e/ege.cy.js
@@ -3,7 +3,7 @@
 describe('Ing. Yngve Ege AS - Main Page', () => {
   it('should load the main page and display key sections', () => {
     // Visit the web page
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
 
     // Check for the main heading
     cy.contains('Løsninger som optimaliserer dine industrielle prosesser').should('be.visible');
@@ -16,7 +16,7 @@ describe('Ing. Yngve Ege AS - Main Page', () => {
   });
 
   it('should display the navigation menu with all main links', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.contains('Produkter').should('be.visible');
     cy.contains('Leverandører').should('be.visible');
     cy.contains('Om oss').should('be.visible');
@@ -25,17 +25,17 @@ describe('Ing. Yngve Ege AS - Main Page', () => {
   });
 
   it('should display the contact phone number with correct link', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.contains('23 24 10 00').should('have.attr', 'href', 'tel:23 24 10 00');
   });
 
   it('should display at least three product images or illustrations', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('section').contains('Våre produkter').parent().find('img, svg').should('have.length.at.least', 3);
   });
 
   it('should display the footer', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('footer').should('exist').and('be.visible');
   });
 }); 
