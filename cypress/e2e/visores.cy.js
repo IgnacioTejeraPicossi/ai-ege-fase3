a2874c91f180928a06c7e2e7d5fcf0a891b6c37f
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2025-05-20 12:56:49 +0200

    Add more visores test

--- cypress/e2e/visores.cy.js
+++ cypress/e2e/visores.cy.js
@@ -1,4 +1,5 @@
-describe('Responsivitetstester', () => {
+
+  describe('Responsivitetstester', () => {
     it('Test på iPhone X', () => {
       cy.viewport('iphone-x');
       cy.visit('/');
@@ -12,7 +13,7 @@ describe('Responsivitetstester', () => {
     });
   
     it('Test på Samsung Galaxy S22', () => {
-      cy.viewport(1080, 2340); // Øppløsning Galaxy S22
+      c.viewport(1080, 2340); // Øppløsning Galaxy S22
       cy.visit('/');
       cy.get('nav').should('be.visible');
     });
@@ -28,11 +29,35 @@ describe('Responsivitetstester', () => {
       cy.visit('/');
       cy.get('nav').should('be.visible');
     });
-
+  
     it('Test på iPad', () => {
       cy.viewport('ipad-mini');
       cy.visit('/');
       cy.get('nav').should('be.visible');
     });
-
+  
+    it('Test på Google Pixel 5, Legger til tester for en populær Android-enhet med standardoppløsning', () => {
+      cy.viewport(1080, 2340); // Oppløsning for Pixel 5
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+  
+  it('Test på Surface Duo - Enheten har et unikt skjermforhold, nyttig for å teste oppdeling og tilpasning', () => {
+      cy.viewport(1350, 1800); // Oppløsning for Surface Duo
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+  
+  it('Test på stor skrivebordsoppløsning (1920x1080), dekker en større skjermstandard for skrivebordsbruk.', () => {
+      cy.viewport(1920, 1080); // Full HD skrivebordsoppløsning
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+  
+  it('Test på ultrabred skjerm (2560x1440), for å teste oppsett på store skjermer eller monitorer med høy oppløsning.', () => {
+      cy.viewport(2560, 1440); // WQHD oppløsning
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+  
   });
